c63c7e8e7688d651461e2088c3e2a838337947bc
Edward Vega <edward.vega@correo.uis.edu.co>
2023-03-22 16:26:57 -0500

    Actualización 1.0 test2-git.py

--- test2-git.py
+++ test2-git.py
@@ -5,3 +5,7 @@ while(i < len(squares) and squares[i] == 'orange'):
     new_squares.append(squares[i])
     i = i + 1
 print (new_squares)
+
+if new_squares != 'orange':
+    print("U have to be strong if u want to be number one")
+else: print("kk")
